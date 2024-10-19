d3484d4d966cf3c99ef716ab245f6edd204dd7bc
Máiréad O'Shea <mairead.kelly26@gmail.com>
2024-10-19 20:06:43 +0000

    header and nav styling

--- found_it/settings.py
+++ found_it/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-yxh-x4293$gf8-_07)&hasl%_u_3u8b&3lt(f$)t+u*4nsn5j0"
+SECRET_KEY = os.getenv('SECRET_KEY', 'default_fallback_secret_key')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
