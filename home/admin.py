afae1b3b9d2ae607f406a6da8bc14d07ca161038
Máiréad O'Shea <mairead.kelly26@gmail.com>
2024-11-15 18:02:12 +0000

    changed to new DB

--- home/admin.py
+++ home/admin.py
@@ -1,19 +1,7 @@
 from django.contrib import admin
-from .models import Cake, Order, Customer, Comment, Rating
+from .models import Cake, Customer, Comment, Rating
 
-# Register your models here.
-@admin.register(Cake)
-class CakeAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description', 'price', 'image', 'slug', 'category')
-    prepopulated_fields = {"slug": ("name",)}
-
-@admin.register(Order)
-class OrderAdmin(admin.ModelAdmin):
-    list_display = ('customer', 'cake', 'quantity', 'status', 'delivery_time')
-
-@admin.register(Customer)
-class CustomerAdmin(admin.ModelAdmin):
-    list_display = ('user', 'address', 'phone_number', 'email')
-
+admin.site.register(Cake)
+admin.site.register(Customer)
 admin.site.register(Comment)
 admin.site.register(Rating)
