636b66c1e63711a653f5307af4c0282ac8f5b727
Máiréad O'Shea <mairead.kelly26@gmail.com>
2024-11-18 20:12:22 +0000

    add categories to products

--- products/models.py
+++ products/models.py
@@ -7,18 +7,21 @@ from cloudinary.models import CloudinaryField
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
+    related_name = models.CharField(max_length=254, null=True, blank=True)
     description = models.TextField(blank=True, null=True)
+    parent = models.ForeignKey("self", on_delete=models.CASCADE, blank=True, null=True, related_name="subcategories")
 
     def __str__(self):
+        if self.parent:
+            return f"{self.parent.name} -> {self.name}"
         return self.name
-
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
     preview_description = models.CharField(max_length=255)  # Use TextField if it's longer
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products")
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, related_name="products", null=True, blank=True)
     image = CloudinaryField("image", blank=True, null=True)
     slug = models.SlugField(unique=True, blank=True)
 
@@ -29,8 +32,8 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
-    
-    
+
+
 class Cake(models.Model):
     OCCASION_CHOICES = [
         ("wedding", "Wedding"),
@@ -46,9 +49,11 @@ class Cake(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = CloudinaryField('image')  # Use CloudinaryField to store images
+    image = CloudinaryField("image")  # Use CloudinaryField to store images
     slug = models.SlugField(unique=True, blank=True)
-    category = models.CharField(max_length=50, choices=OCCASION_CHOICES, default="other")
-    
+    category = models.CharField(
+        max_length=50, choices=OCCASION_CHOICES, default="other"
+    )
+
     def __str__(self):
         return self.name
