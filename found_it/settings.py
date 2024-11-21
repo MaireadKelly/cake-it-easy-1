378dece43fa7473417a4bf44c002e06be8da4448
Máiréad O'Shea <mairead.kelly26@gmail.com>
2024-11-21 17:14:17 +0000

    update data for deploy

--- found_it/settings.py
+++ found_it/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "default_fallback_secret_key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-maireadkelly-foundit-vs9ytjoojur.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-maireadkelly-foundit-vs9ytjoojur.ws.codeinstitute-ide.net', '.herokuapp.com']
 
 
 # Application definition
