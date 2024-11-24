804e1d7294bdb63238e2129c470e8b9cf1684a45
Máiréad O'Shea <mairead.kelly26@gmail.com>
2024-11-24 17:34:32 +0000

    edit cakes/model to handle unique slug generation

--- products/models.py
+++ products/models.py
@@ -38,6 +38,20 @@ class Cake(models.Model):
     def __str__(self):
         return self.name
     
+    def generate_unique_slug(self):
+        base_slug = slugify(self.name)
+        slug = base_slug
+        counter = 1
+        while Cake.objects.filter(slug=slug).exists():
+            slug = f"{base_slug}-{counter}"
+            counter += 1
+        return slug
+
+def save(self, *args, **kwargs):
+    if not self.slug:
+        self.slug = self.generate_unique_slug()
+    super().save(*args, **kwargs)
+    
     
 class CustomCake(models.Model):
     FLAVOR_CHOICES = [
