1b7efc67c4ddd4b645a43262aa68d9b65576bb7e
Máiréad O'Shea <mairead.kelly26@gmail.com>
2024-11-24 16:28:20 +0000

    Add slug to custom cake

--- products/models.py
+++ products/models.py
@@ -61,6 +61,7 @@ class CustomCake(models.Model):
     inscription = models.CharField(max_length=255, blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = CloudinaryField("image", blank=True, null=True)
+    slug = models.SlugField(unique=True, blank=True)
 
     def __str__(self):
         return f"Custom Cake - {self.flavor} with {self.filling} filling"
