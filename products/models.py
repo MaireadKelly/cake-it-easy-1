7b16d7d185e78559e448d14458b64b337af360c3
Máiréad O'Shea <mairead.kelly26@gmail.com>
2024-11-30 13:54:24 +0000

    edit cake model size

--- products/models.py
+++ products/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils.text import slugify
 from cloudinary.models import CloudinaryField
 
+
 class Category(models.Model):
     class Meta:
         verbose_name_plural = "Categories"
@@ -9,7 +10,11 @@ class Category(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
     parent = models.ForeignKey(
-        "self", on_delete=models.CASCADE, blank=True, null=True, related_name="subcategories"
+        "self",
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+        related_name="subcategories",
     )
 
     def __str__(self):
@@ -72,6 +77,15 @@ class Cake(models.Model):
         super().save(*args, **kwargs)
 
 
+class CakeSize(models.Model):
+    name = models.CharField(max_length=50)  # E.g., "Small," "Medium," "Large"
+    description = models.TextField(blank=True, null=True)  # Optional details
+    price = models.DecimalField(max_digits=6, decimal_places=2)  # Price for this size
+
+    def __str__(self):
+        return f"{self.name} - €{self.price}"
+
+
 class CustomCake(models.Model):
     FLAVOR_CHOICES = [
         ("vanilla", "Vanilla"),
@@ -90,7 +104,9 @@ class CustomCake(models.Model):
     ]
 
     flavor = models.CharField(max_length=50, choices=FLAVOR_CHOICES, default="vanilla")
-    filling = models.CharField(max_length=50, choices=FILLING_CHOICES, default="buttercream")
+    filling = models.CharField(
+        max_length=50, choices=FILLING_CHOICES, default="buttercream"
+    )
     inscription = models.CharField(max_length=255, blank=True, null=True)
     sizes = models.ManyToManyField(CakeSize, related_name="custom_cakes", blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
@@ -108,13 +124,19 @@ class Product(models.Model):
         ("other", "Other"),
     ]
 
-    product_type = models.CharField(max_length=50, choices=PRODUCT_TYPE_CHOICES, default="cake")
+    product_type = models.CharField(
+        max_length=50, choices=PRODUCT_TYPE_CHOICES, default="cake"
+    )
     name = models.CharField(max_length=255)
     preview_description = models.CharField(max_length=255, blank=True, null=True)
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey(
-        Category, on_delete=models.SET_NULL, related_name="products", null=True, blank=True
+        Category,
+        on_delete=models.SET_NULL,
+        related_name="products",
+        null=True,
+        blank=True,
     )
     image = CloudinaryField("image", blank=True, null=True)
     slug = models.SlugField(unique=True, blank=True)
