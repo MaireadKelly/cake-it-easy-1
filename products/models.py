4359fb75882fe6d6b43323345998593115e7ea52
Máiréad O'Shea <mairead.kelly26@gmail.com>
2024-11-30 20:25:27 +0000

    Revert "remove duplicate CakeSize model"
    This reverts commit bcc854475fb0246895e068b591efc0ea748dec1e.

--- products/models.py
+++ products/models.py
@@ -23,11 +23,10 @@ class Category(models.Model):
         return self.name
 
 
-
 class CakeSize(models.Model):
-    name = models.CharField(max_length=50)  # E.g., "Small," "Medium," "Large"
-    description = models.TextField(blank=True, null=True)  # Optional details
-    price = models.DecimalField(max_digits=6, decimal_places=2)  # Price for this size
+    name = models.CharField(max_length=50)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
 
     def __str__(self):
         return f"{self.name} - €{self.price}"
@@ -76,6 +75,15 @@ class Cake(models.Model):
         if not self.slug:
             self.slug = self.generate_unique_slug()
         super().save(*args, **kwargs)
+
+
+class CakeSize(models.Model):
+    name = models.CharField(max_length=50)  # E.g., "Small," "Medium," "Large"
+    description = models.TextField(blank=True, null=True)  # Optional details
+    price = models.DecimalField(max_digits=6, decimal_places=2)  # Price for this size
+
+    def __str__(self):
+        return f"{self.name} - €{self.price}"
 
 
 class CustomCake(models.Model):
