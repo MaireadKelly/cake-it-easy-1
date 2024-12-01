42ccce31261ed0ad865afc74fd84e0a6f4708384
Máiréad O'Shea <mairead.kelly26@gmail.com>
2024-12-01 11:47:18 +0000

    change url

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from . import views, webhooks
+from . import views, webhook
 
 urlpatterns = [
     path('', views.checkout, name='checkout'),  # Checkout view
     path('checkout_success/<order_number>', views.checkout_success, name='checkout_success'),
     path('cache_checkout_data/', views.cache_checkout_data, name='cache_checkout_data'),
-    path('wh/', webhooks, name='webhook'),
+    path('wh/', webhook, name='webhook'),
     path('order-confirmation/<str:order_number>/', views.order_confirmation, name='order_confirmation'),  # Order confirmation view
 ]
